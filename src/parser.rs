88f57d546da4ec17632d32f86e35180e7f450644
statiolake <statiolake@gmail.com>
2021-06-09 01:25:33 +0900

    Fix clippy warning

--- src/parser.rs
+++ src/parser.rs
@@ -16,7 +16,7 @@ impl<'a> Parser<'a> {
         self.ptr == self.input.len()
     }
 
-    pub fn peek(&self, n: usize) -> Option<Token<'a>> {
+    pub fn lookahead(&self, n: usize) -> Option<Token<'a>> {
         if self.ptr + n < self.input.len() {
             Some(self.input[self.ptr + n])
         } else {
@@ -24,8 +24,8 @@ impl<'a> Parser<'a> {
         }
     }
 
-    pub fn next(&mut self) -> Option<Token> {
-        let res = self.peek(0);
+    pub fn next_token(&mut self) -> Option<Token> {
+        let res = self.lookahead(0);
         if !self.is_finished() {
             self.ptr += 1;
         }
@@ -35,13 +35,13 @@ impl<'a> Parser<'a> {
 
     #[track_caller]
     pub fn eat(&mut self, expect: Token) {
-        assert_eq!(self.next(), Some(expect), "Eat failed");
+        assert_eq!(self.next_token(), Some(expect), "Eat failed");
     }
 }
 
 impl Parser<'_> {
     pub fn parse_stmt(&mut self) -> AstStmt {
-        match self.peek(0) {
+        match self.lookahead(0) {
             Some(Token::If) => AstStmt::IfStmt(Box::new(self.parse_if_stmt())),
             Some(Token::While) => AstStmt::WhileStmt(Box::new(self.parse_while_stmt())),
             Some(Token::Begin) => AstStmt::BeginStmt(Box::new(self.parse_begin_stmt())),
@@ -84,7 +84,7 @@ impl Parser<'_> {
 
     fn parse_stmt_list(&mut self) -> AstStmtList {
         let stmt = Box::new(self.parse_stmt());
-        let next = if self.peek(0) == Some(Token::Semicolon) {
+        let next = if self.lookahead(0) == Some(Token::Semicolon) {
             self.eat(Token::Semicolon);
             Some(Box::new(self.parse_stmt_list()))
         } else {
@@ -119,7 +119,7 @@ impl Parser<'_> {
 
     fn parse_compare_op(&mut self) -> AstCompareOp {
         match self
-            .next()
+            .next_token()
             .expect("unexpected EOF while reading compare-op")
         {
             Token::Lt => AstCompareOp::Lt,
@@ -133,7 +133,7 @@ impl Parser<'_> {
     }
 
     fn parse_arith_expr(&mut self) -> AstArithExpr {
-        match self.peek(0) {
+        match self.lookahead(0) {
             Some(Token::OpenPar) => {
                 self.eat(Token::OpenPar);
                 let lhs = Box::new(self.parse_arith_expr());
@@ -149,7 +149,7 @@ impl Parser<'_> {
             Some(Token::Ident(_)) => {
                 // peek next identifiers to determine whether it's fncall or variable, look one
                 // token ahead.
-                if self.peek(1) == Some(Token::OpenPar) {
+                if self.lookahead(1) == Some(Token::OpenPar) {
                     // fncall
                     let fncall = Box::new(self.parse_fncall());
                     AstArithExpr::FnCall(fncall)
@@ -173,11 +173,11 @@ impl Parser<'_> {
     }
 
     fn parse_argument_list(&mut self) -> AstArgumentList {
-        match self.peek(0) {
+        match self.lookahead(0) {
             Some(Token::ClosePar) => AstArgumentList::Empty,
             Some(_) => {
                 let expr = Box::new(self.parse_arith_expr());
-                let next = if self.peek(0) == Some(Token::Comma) {
+                let next = if self.lookahead(0) == Some(Token::Comma) {
                     self.eat(Token::Comma);
                     Box::new(self.parse_argument_list())
                 } else {
@@ -191,7 +191,10 @@ impl Parser<'_> {
     }
 
     fn parse_arith_op(&mut self) -> AstArithOp {
-        match self.next().expect("unexpected EOF while reading arith-op") {
+        match self
+            .next_token()
+            .expect("unexpected EOF while reading arith-op")
+        {
             Token::Add => AstArithOp::Add,
             Token::Sub => AstArithOp::Sub,
             Token::Mul => AstArithOp::Mul,
@@ -202,7 +205,7 @@ impl Parser<'_> {
 
     fn parse_const(&mut self) -> AstConst {
         let token = self
-            .next()
+            .next_token()
             .expect("unexpected EOF while reading const value");
         let value = match token {
             Token::Number(value) => value,
@@ -217,7 +220,7 @@ impl Parser<'_> {
     }
 
     fn parse_ident(&mut self) -> AstIdent {
-        let token = self.next().expect("unexpected EOF while reading var");
+        let token = self.next_token().expect("unexpected EOF while reading var");
         let ident = match token {
             Token::Ident(ident) => ident,
             other => panic!("unexpected token {:?} for var", other),
